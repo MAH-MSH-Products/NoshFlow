cad014bf0ca85729ec2de8678ad286bf0bb79551
Mohammad <m.a.hajialirezaei05@gmail.com>
2026-07-29 17:14:25 +0330

    chore: update vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js', // We will create this file next
-    css: true,
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
